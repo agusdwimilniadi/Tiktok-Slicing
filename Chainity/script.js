46fe99dafa2099243d9a6046026fc571e4de63db
agusdwimilniadi <agusdwimill@gmail.com>
2022-12-23 23:07:33 +0700

    initial commit

---
+++ Chainity/script.js
@@ -0,0 +1,19 @@
+let myNavbar = document.getElementById("myNavbar");
+
+// OnScroll event handler
+function onScroll2() {
+  // Get scroll value
+  const scroll = document.documentElement.scrollTop;
+
+  // If scroll value is more than 0 - add class
+  if (scroll > 100) {
+    myNavbar.classList.add("bg-dark");
+    myNavbar.classList.remove("bg-transparent");
+  } else {
+    myNavbar.classList.remove("bg-dark");
+    myNavbar.classList.add("bg-transparent");
+  }
+}
+
+// Use the function
+window.addEventListener("scroll", onScroll2);
